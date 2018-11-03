9c6176160a684b00fb7fb37f7800a19c5bb5d46f
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2018-11-03 15:49:10 -0500

    Update rps.js

--- commands/rps.js
+++ commands/rps.js
@@ -10,6 +10,53 @@ module.exports.run = async (bot, message, args) => {
   let rps = 0;
   rps = randomnum(1,3);
 
+  return message.channel.send("Rock,").then((message) => {
+    message.edit("Paper,")
+    message.edit("Scissors,")
+    message.edit("Shoot!")
+
+    if (args[0] === "paper"){
+      if (rps == 1){
+        message.edit("I choose Scissors :scissors:! Yes, I won! :grinning: ")
+      }else if (rps == 2){
+        message.edit("I choose Paper :page_facing_up:! Tie! :astonished: ")
+      }else if (rps == 3){
+        message.edit("I choose Rock <:rock:503683039130419201>! Oh no, I lost! :cry: ")
+      }
+    };
+
+  if (args[0] === "rock"){
+    if (rps == 1){
+      message.edit("I choose Scissors :scissors:! Oh no, I lost! :cry:")
+    }else if (rps == 2){
+      message.edit("I choose Paper :page_facing_up:! Yes, I won! :grinning: ")
+    }else if (rps == 3){
+      message.edit("I choose Rock <:rock:503683039130419201>! Tie! :astonished: ")
+    }
+  };
+
+  if (args[0] === "paper"){
+    if (rps == 1){
+      message.edit("I choose Scissors :scissors:! Yes, I won! :grinning: ")
+    }else if (rps == 2){
+      message.edit("I choose Paper :page_facing_up:! Tie! :astonished: ")
+    }else if (rps == 3){
+      message.edit("I choose Rock <:rock:503683039130419201>! Oh no, I lost! :cry: ")
+    }
+  };
+
+  if (args[0] === "scissors"){
+    if (rps == 1){
+      message.edit("I choose Scissors :scissors:! Tie! :astonished: ")
+    }else if (rps == 2){
+      message.edit("I choose Paper :page_facing_up:! Oh no, I lost! :cry: ")
+    }else if (rps == 3){
+      message.edit("I choose Rock <:rock:503683039130419201>! Yes, I won! :grinning: ")
+    }
+  };
+
+});
+
   if (args[0] === "rock"){
     if (rps == 1){
       message.channel.send("I choose Scissors :scissors:!")
