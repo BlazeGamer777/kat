020055f07adb639806d37e3529950662e0d38549
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2018-11-11 20:41:48 -0600

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -5,22 +5,22 @@ module.exports.run = async (bot, message, args) => {
   if(args[0] == "help") return message.reply(`Usage: \`k.weather <location>\``);
   weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
     if (err) console.log(err);
-    let current = result[0].current;
+    let ccurrent = result[0].current;
     let location = result[0].location;
     if (result.length === 0) {
       return message.channel.send("Please send a ***valid*** location!")
     };
     let embed = new Discord.RichEmbed()
-        .setDescription(`**${current.skytext}**`)
-        .setAuthor(`Weather for ${current.observationpoint}`)
-        .setThumbnail(current.imageUrl)
+        .setDescription(`**${ccurrent.skytext}**`)
+        .setAuthor(`Weather for ${ccurrent.observationpoint}`)
+        .setThumbnail(ccurrent.imageUrl)
         .setColor('#1569C7')
         .addField('Timezone', `UTC${location.timezone}`, true)
         .addField('Degree Type', location.degreetype, true)
-        .addField('Temperature', `${current.temperature} Degrees`, true)
-        .addField('Feels Like', `${current.feelslike} Degrees`, true)
-        .addField('Winds', current.winddisplay, true)
-        .addField('Humidity', `${current.humidity}%`, true);
+        .addField('Temperature', `${ccurrent.temperature} Degrees`, true)
+        .addField('Feels Like', `${ccurrent.feelslike} Degrees`, true)
+        .addField('Winds', ccurrent.winddisplay, true)
+        .addField('Humidity', `${ccurrent.humidity}%`, true);
 
     message.channel.send(embed);
 
