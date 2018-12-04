1c5e12182623ea76d75870476c240f226bda1fdb
BlazeGamer777 <42978164+BlazeGamer777@users.noreply.github.com>
2018-12-04 17:21:39 -0600

    Create emote.js

---
+++ commands/emote.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let name = args[0]
+  let imgId = args[1]
+  
+  message.channel.send('<a:' + name + ':' + imgId '>');
+}
+
+module.exports.help = {
+  name: "emote"
+};
